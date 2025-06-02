d1fc1c83edd98205b9352622444698affb8367cc
Robert Dizor <71193617+rdizor@users.noreply.github.com>
2025-06-02 14:18:06 -0500

    Removed "using Unity.Sentis" import cmd
    /*
    Removed "using Unity.Sentis" import cmd
    “Fork without Sentis for Unity Exoskeleton Project” 
    */

--- com.unity.ml-agents/Runtime/Inference/DynamicTensorShapeExtensions.cs
+++ com.unity.ml-agents/Runtime/Inference/DynamicTensorShapeExtensions.cs
@@ -1,4 +1,7 @@
-using Unity.Sentis;
+/*
+Removed "using Unity.Sentis" import cmd
+“Fork without Sentis for Unity Exoskeleton Project” 
+*/
 using UnityEngine.Assertions;
 
 namespace Unity.MLAgents.Inference
